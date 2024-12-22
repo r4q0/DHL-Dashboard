88d9f6601ac09aa86fcaa88951ed76a4f6f301b8
r5q0 <bilalemailsss@gmail.com>
2024-12-22 14:24:56 +0100

    Update button styles and text sizes in create and welcome views for improved UI consistency

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -10,9 +10,9 @@
 </head>
 
 <body class="m-5 h-screen text-slate-400 bg-slate-900 overflow-hidden ">
-    <button onclick="location.href='{{route('welcome')}}';"  type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none  focus:ring-blue-800 shadow-lg shadow-blue-800/80 rounded-lg text-2xl px-3 py-1 font-black text-center me-2 mb-2 ">Back</button>   
+    <button onclick="location.href='{{route('welcome')}}';"  type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none  focus:ring-blue-800 shadow-lg shadow-blue-800/80 rounded-lg text-lg lg:text-2xl px-3 py-1 font-black text-center me-2 mb-2 ">Back</button>   
     <div class="flex items-center justify-center overflow-hidden m-5 text-slate-400 bg-slate-900">
-    <div class="hover:cursor-pointer font-mono font-medium leading-6 text-sky-500 hover:font-black whitespace-nowrap hover:border-slate-200 text-sky-400 border border-slate-100 dark:border-slate-400/10 text-center font-mono font-semibold text-3xl leading-6 text-sky-500 whitespace-nowrap text-sky-400 text-center  m-5 p-20 rounded-lg">
+    <div class="hover:cursor-pointer font-mono font-medium leading-6 text-sky-500 hover:font-black whitespace-nowrap hover:border-slate-200 text-sky-400 border border-slate-100 dark:border-slate-400/10 text-center font-mono font-semibold text-lg md:text-3xl leading-6 text-sky-500 whitespace-nowrap text-sky-400 text-center  m-5 p-20 rounded-lg">
         <form action="" method="post">
             @csrf
             <input type="text" placeholder="name" name="name"
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,13 +10,13 @@
 </head>
 
 <body class="m-5 h-screen text-slate-400 bg-slate-900">
-   <button onclick="location.href='{{route('create')}}';"  type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none  focus:ring-blue-800 shadow-lg shadow-blue-800/80 rounded-lg text-2xl px-3 py-1 font-black text-center me-2 mb-2 ">&plus;</button>   
+   <button onclick="location.href='{{route('create')}}';"  type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none  focus:ring-blue-800 shadow-lg shadow-blue-800/80 rounded-lg text-lg md:text-2xl px-3 py-1 font-black text-center me-2 mb-2 ">&plus;</button>   
     <div class="flex justify-center antialiased text-slate-400 bg-slate-900">
     <div class="grid grid-cols-1 gap-10">
         @foreach ($orders as $order)
             <div onclick="location.href='{{ route('order', ['id' => $order->id]) }}';" class="hover:cursor-pointer   py-2 px-5 font-mono font-medium text-xs leading-6 text-sky-500 hover:font-black whitespace-nowrap hover:border-slate-200 text-sky-400 border border-slate-100 dark:border-slate-400/10">
-                <h1  class="py-2 pr-2 text-center font-mono font-semibold text-3xl leading-6 text-sky-500 whitespace-nowrap text-sky-400">{{ $order->name }}</h1>
-                <p class="text-center text-2xl p-1 font-mono whitespace-pre text-indigo-300 leading-6 text-gray-500">{{ $order->tracking_number }} {{ $order->zipcode }}</p>
+                <h1  class="py-2 pr-2 text-center font-mono font-semibold text-2xl md:text-3xl leading-6 text-sky-500 whitespace-nowrap text-sky-400">{{ $order->name }}</h1>
+                <p class="text-center text-md md:text-2xl p-1 font-mono whitespace-pre text-indigo-300 leading-6 text-gray-500">{{ $order->tracking_number }} {{ $order->zipcode }}</p>
             </div>
         @endforeach
     </div>
