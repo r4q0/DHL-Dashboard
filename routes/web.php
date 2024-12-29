fad92859506154a3fbeb807fda2aced63b8f5874
r5q0 <bilalemailsss@gmail.com>
2024-12-29 14:21:20 +0100

    Add basic authentication middleware and update environment configuration

--- routes/web.php
+++ routes/web.php
@@ -3,28 +3,31 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DhlController;
 use App\Http\Controllers\RenderController;
+use Olssonm\VeryBasicAuth\Http\Middleware\VeryBasicAuth;
 use Illuminate\Http\Request;
 
-Route::get('/test', function () {
-    return DhlController::scrapeDHL('JJD000090254011014512771', '8011CJ');
+Route::group(['middleware' => 'auth.very_basic'], function () {
+    Route::get('/test', function () {
+        return DhlController::scrapeDHL('JJD000090254011014512771', '8011CJ');
+    });
+
+    Route::get('/order/{id}', function ($id) {
+        return RenderController::renderOrder($id);
+    })->name('order')->middleware(VeryBasicAuth::class);
+
+    Route::get('/', function () {
+        return RenderController::renderDashboard();
+    })->name('welcome');
+
+    Route::get('/create', function () {
+        return RenderController::renderCreate();
+    })->name('create');
+
+    Route::post('/create', function (Request $request) {
+        return RenderController::create($request);
+    });
+
+    Route::get('/remove/{id}', function ($id) {
+        return RenderController::removeOrder($id);
+    })->name('remove');
 });
-
-Route::get('/order/{id}', function ($id) {
-    return RenderController::renderOrder($id);
-})->name('order');
-
-Route::get('/', function () {
-    return RenderController::renderDashboard();
-})->name('welcome');
-
-Route::get('/create', function () {
-    return RenderController::renderCreate();
-})->name('create');
-
-Route::post('/create', function (Request $request) {
-    return RenderController::create($request);
-});
-
-Route::get('/remove/{id}', function ($id) {
-    return RenderController::removeOrder($id);
-})->name('remove');
